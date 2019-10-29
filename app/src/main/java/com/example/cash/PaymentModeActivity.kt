c0d13b1f655307e8406963eed22400c79dcdf474
natdev <dordonne.nathanael@gmail.com>
2019-10-28 21:35:14 -0400

    first commit

---
+++ app/src/main/java/com/example/cash/PaymentModeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PaymentModeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_mode)
+    }
+}
---
+++ app/src/main/java/com/example/cash/PaymentViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.cash
+
+import androidx.lifecycle.ViewModel
+
+class PaymentViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
